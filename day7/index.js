5422b8a4acc9ec00ce05e00e99e72e2fa8a71cf0
Maxim Orlov <maximorl@gmail.com>
2020-12-07 12:27:30 +0100

    Refactor day 7 a little

--- day7/index.js
+++ day7/index.js
@@ -2,37 +2,35 @@
 const fs = require('fs');
 const path = require('path');
 const input = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8');
+const bagRules = input.split('.\n');
 
 /* Part 1 */
-const bagsRules = input.split('.\n');
-
-function traverseBagRulesPart1(bagColor) {
-  const bagRule = bagsRules.find((bagRule) => bagRule.startsWith(bagColor));
+function hasShinyGoldBag(bagColor) {
+  const bagRule = bagRules.find((bagRule) => bagRule.startsWith(bagColor));
   if (bagRule.endsWith('no other bags')) {
     return false;
   }
 
-  const [, childrenBag] = bagRule.split(' bags contain ');
-  const childrenBagColors = childrenBag.split(', ')
-    .map((childrenBagColor) => childrenBagColor.match(/^\d (\w.*?) bag|bags$/)[1]);
+  const [, childrenBags] = bagRule.split(' bags contain ');
+  const childrenBagColors = childrenBags.split(', ')
+    .map((childBagSpecification) => childBagSpecification.match(/^\d (\w.*?) bag|bags$/)[1]);
 
   if (childrenBagColors.includes('shiny gold')) {
     return true;
   }
 
-  return childrenBagColors.some((childrenBagColor) => traverseBagRulesPart1(childrenBagColor));
+  return childrenBagColors.some(hasShinyGoldBag);
 };
 
-const bagsThatCanHoldMyBag = bagsRules
-  .filter((bagRule) => {
-    const [bagColor] = bagRule.split(' bags contain ');
-    return traverseBagRulesPart1(bagColor);
-  });
+const bagsThatCanHoldMyBag = bagRules.filter((bagRule) => {
+  const [bagColor] = bagRule.split(' bags contain ');
+  return hasShinyGoldBag(bagColor);
+});
 console.log('Answer part 1:', bagsThatCanHoldMyBag.length);
 
 /* Part 2 */
-function traverseBagRulesPart2(bagColor) {
-  const bagRule = bagsRules.find((bagRule) => bagRule.startsWith(bagColor));
+function getNoOfBags(bagColor) {
+  const bagRule = bagRules.find((bagRule) => bagRule.startsWith(bagColor));
   if (bagRule.endsWith('no other bags')) {
     return 0;
   }
@@ -40,11 +38,10 @@ function traverseBagRulesPart2(bagColor) {
   const [, childrenBag] = bagRule.split(' bags contain ');
   const childrenBagSpecifications = childrenBag.split(', ');
 
-  return childrenBagSpecifications
-    .reduce((sumOfBags, childrenBagSpecification) => {
-      const [, noOfBags, childrenBagColor] = childrenBagSpecification.match(/(^\d) (\w.*?) bag|bags$/);
-      return sumOfBags + Number(noOfBags) + Number(noOfBags) * traverseBagRulesPart2(childrenBagColor);
-    }, 0);
+  return childrenBagSpecifications.reduce((sumOfBags, childBagSpecification) => {
+    const [, noOfBags, childBagColor] = childBagSpecification.match(/(^\d) (\w.*?) bag|bags$/);
+    return sumOfBags + Number(noOfBags) + Number(noOfBags) * getNoOfBags(childBagColor);
+  }, 0);
 };
 
-console.log('Answer part 2:', traverseBagRulesPart2('shiny gold'));
+console.log('Answer part 2:', getNoOfBags('shiny gold'));
