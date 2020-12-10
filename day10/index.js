49e127b1cc080373d3e6a26b4dc03b8c85f69e1f
Maxim Orlov <maximorl@gmail.com>
2020-12-10 15:28:02 +0100

    Day 10 part 2 solved! Memoization did the trick

--- day10/index.js
+++ day10/index.js
@@ -4,40 +4,35 @@ const path = require('path');
 const input = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8');
 const adapters = input.split('\n').map(Number);
 adapters.sort((a, b) => a - b);
+const adaptersWithOutlet = [0, ...adapters];
 
 /* Part 1 */
-const sortedJoltDifferences = adapters
-  .map((adapter, index) => adapters[index + 1] ? adapters[index + 1] - adapter : 3);
-sortedJoltDifferences.unshift(adapters[0] - 0);
+const sortedJoltDifferences = adaptersWithOutlet
+  .map((adapter, index) => adaptersWithOutlet[index + 1] ? adaptersWithOutlet[index + 1] - adapter : 3);
 
 const oneJoltDifferences = sortedJoltDifferences.filter((joltDifference) => joltDifference === 1);
 const threeJoltDifferences = sortedJoltDifferences.filter((joltDifference) => joltDifference === 3);
 console.log('Answer part 1:', oneJoltDifferences.length * threeJoltDifferences.length);
 
 /* Part 2 */
-// const graph = Object.fromEntries(
-//   adapters
-//     .map((adapter, index, sortedAdapters) => {
-//       const childrenAdapters = sortedAdapters.slice(index + 1, index + 4)
-//         .filter((nextAdapter) => nextAdapter - adapter <= 3);
-//       const adapterNode = [adapter, childrenAdapters];
-//       return adapterNode;
-//     })
-// );
+const graph = Object.fromEntries(
+  adaptersWithOutlet.map((adapter, index) => {
+    const childrenAdapters = adaptersWithOutlet.slice(index + 1, index + 4)
+      .filter((nextAdapter) => nextAdapter - adapter <= 3);
+    return [adapter, childrenAdapters];
+  })
+);
 
+let memoization = {};
 function numberOfArrangements(adapter) {
-  const index = adapters.indexOf(adapter);
-  const childrenAdapters = adapters.slice(index + 1, index + 4)
-    .filter((nextAdapter) => nextAdapter - adapter <= 3);
-
-  if (childrenAdapters.length === 0) {
-    return 1;
-  } else if (childrenAdapters.length === 1) {
-    return numberOfArrangements(childrenAdapters[0]);
-  } else if (childrenAdapters.length === 2) {
-    return numberOfArrangements(childrenAdapters[0]) + numberOfArrangements(childrenAdapters[1]);
-  } else {
-    return numberOfArrangements(childrenAdapters[0]) + numberOfArrangements(childrenAdapters[1]) + numberOfArrangements(childrenAdapters[2]);
+  if (!memoization[adapter]) {
+    const childrenAdapters = graph[adapter];
+    memoization[adapter] = childrenAdapters.length
+      ? childrenAdapters.reduce((acc, childAdapter) => acc + numberOfArrangements(childAdapter), 0)
+      : 1;
   }
+
+  return memoization[adapter];
 }
-console.log('Answer part 2:', numberOfArrangements(adapters[0]));
+
+console.log('Answer part 2:', numberOfArrangements(0));
