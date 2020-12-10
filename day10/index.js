0d6b5e9cd9f34c73692b4bfe800ffb7b552e65fe
Maxim Orlov <maximorl@gmail.com>
2020-12-10 12:57:35 +0100

    Fibonacci attempt at day 10 still taking too long..

--- day10/index.js
+++ day10/index.js
@@ -7,7 +7,7 @@ adapters.sort((a, b) => a - b);
 
 /* Part 1 */
 const sortedJoltDifferences = adapters
-  .map((adapter, index, adapters) => adapters[index + 1] ? adapters[index + 1] - adapter : 3);
+  .map((adapter, index) => adapters[index + 1] ? adapters[index + 1] - adapter : 3);
 sortedJoltDifferences.unshift(adapters[0] - 0);
 
 const oneJoltDifferences = sortedJoltDifferences.filter((joltDifference) => joltDifference === 1);
@@ -15,27 +15,29 @@ const threeJoltDifferences = sortedJoltDifferences.filter((joltDifference) => jo
 console.log('Answer part 1:', oneJoltDifferences.length * threeJoltDifferences.length);
 
 /* Part 2 */
-const graph = Object.fromEntries(
-  adapters
-    .map((adapter, index, sortedAdapters) => {
-      const childrenAdapters = sortedAdapters.slice(index + 1, index + 4)
-        .filter((nextAdapter) => nextAdapter - adapter <= 3);
-      const adapterNode = [adapter, childrenAdapters];
-      return adapterNode;
-    })
-);
+// const graph = Object.fromEntries(
+//   adapters
+//     .map((adapter, index, sortedAdapters) => {
+//       const childrenAdapters = sortedAdapters.slice(index + 1, index + 4)
+//         .filter((nextAdapter) => nextAdapter - adapter <= 3);
+//       const adapterNode = [adapter, childrenAdapters];
+//       return adapterNode;
+//     })
+// );
 
-let numberOfArrangements = 0;
-function traverseGraph(adapter) {
-  const childrenAdapters = graph[adapter];
+function numberOfArrangements(adapter) {
+  const index = adapters.indexOf(adapter);
+  const childrenAdapters = adapters.slice(index + 1, index + 4)
+    .filter((nextAdapter) => nextAdapter - adapter <= 3);
+
   if (childrenAdapters.length === 0) {
-    numberOfArrangements++;
-    if (numberOfArrangements % 10000000 === 0) {
-      console.log(numberOfArrangements);
-    }
-    return;
+    return 1;
+  } else if (childrenAdapters.length === 1) {
+    return numberOfArrangements(childrenAdapters[0]);
+  } else if (childrenAdapters.length === 2) {
+    return numberOfArrangements(childrenAdapters[0]) + numberOfArrangements(childrenAdapters[1]);
+  } else {
+    return numberOfArrangements(childrenAdapters[0]) + numberOfArrangements(childrenAdapters[1]) + numberOfArrangements(childrenAdapters[2]);
   }
-
-  return childrenAdapters.forEach(traverseGraph);
 }
-console.log('Answer part 2:', traverseGraph(adapters[0]));
+console.log('Answer part 2:', numberOfArrangements(adapters[0]));
