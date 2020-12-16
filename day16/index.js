890c40787b67dad18457f3886f1a36eb682b5649
Maxim Orlov <maximorl@gmail.com>
2020-12-16 14:22:36 +0100

    Added comments for Day 16

--- day16/index.js
+++ day16/index.js
@@ -2,24 +2,50 @@
 const fs = require('fs');
 const path = require('path');
 const input = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8');
-const [unparsedFields, ourTicket, unparsedNearbyTickets] = input.split('\n\n');
+const [unparsedFields, unparsedOurTicket, unparsedNearbyTickets] = input.split('\n\n');
 
-const fields = unparsedFields.split('\n').map((field) => {
-  let [fieldName, fieldBoundaries] = field.split(': ');
-  fieldBoundaries = fieldBoundaries.split(' or ')
-    .flatMap((fieldBoundary) => fieldBoundary.split('-'))
-    .map(Number);
+/*
+ * Parsing fields, our ticket and nearby tickets
+ */
 
-  return [fieldName, fieldBoundaries];
-});
+// Fields
+// [
+//   [ departure location, [30, 828, 839, 971] ],
+//   [ departure station, [38, 339, 352, 958] ],
+//   ...
+// ]
+//
+const fields = unparsedFields.split('\n')
+  .map((field) => {
+    let [fieldName, fieldBoundaries] = field.split(': ');
+    fieldBoundaries = fieldBoundaries.split(' or ')
+      .flatMap((fieldBoundary) => fieldBoundary.split('-'))
+      .map(Number);
 
-const [, ...nearbyTickets] = unparsedNearbyTickets.split('\n')
-  .map((ticket) => ticket.split(','));
+    return [fieldName, fieldBoundaries];
+  });
+
+// Our ticket
+// [139, 113, 127, ... ]
+const ourTicket = unparsedOurTicket.split('\n')[1].split(',');
+
+// Nearby tickets
+// [
+//   [475, 649, 830, ...],
+//   [888, 593, 683, ...],
+//   ...
+// ]
+const nearbyTickets = unparsedNearbyTickets.split('\n')
+  .slice(1)
+  .map((ticket) => ticket.split(',').map(Number));
 
 /* Part 1 */
 const sumOfInvalidValues = nearbyTickets
+  // Get all invalid values from all tickets in a flattened array
   .flatMap((ticket) => {
-    return ticket.map(Number).filter((value) => {
+    // Filter out invalid values for this ticket
+    return ticket.filter((value) => {
+      // Return true if value is invalid for all fields
       return !fields.some(([, fieldBoundaries]) => {
         const [
           lowerBoundaryFirstHalf,
@@ -28,6 +54,7 @@ const sumOfInvalidValues = nearbyTickets
           upperBoundarySecondHalf,
         ] = fieldBoundaries;
 
+        // Return true if value is valid for current field
         return (value >= lowerBoundaryFirstHalf && value <= upperBoundaryFirstHalf)
           || (value >= lowerBuondarySecondHalf && value <= upperBoundarySecondHalf);
       })
@@ -38,7 +65,9 @@ console.log('Answer part 1:', sumOfInvalidValues);
 
 /* Part 2 */
 const validTickets = nearbyTickets.filter((ticket) => {
-  return ticket.map(Number).every((value) => {
+  // Return true if all values in this ticket are valid
+  return ticket.every((value) => {
+      // Return true if value if valid for at least one field
       return fields.some(([, fieldBoundaries]) => {
         const [
           lowerBoundaryFirstHalf,
@@ -53,18 +82,28 @@ const validTickets = nearbyTickets.filter((ticket) => {
     });
 });
 
-const fieldsToValues = validTickets.reduce((fieldsToValues, ticket) => {
+// An object with keys denoting the position in the ticket and value all numbers at that position
+// {
+//   '0': [475, 888, 371, ...],
+//   '1': [649, 593, 683, ...],
+//   ...
+// }
+const valuesForPosition = validTickets.reduce((fieldsToValues, ticket) => {
   ticket.forEach((value, index) => {
-    fieldsToValues[index] = fieldsToValues[index]
-      ? fieldsToValues[index].concat(value)
-      : [value];
+    fieldsToValues[index] = fieldsToValues[index] ? fieldsToValues[index].concat(value) : [value];
   });
 
   return fieldsToValues;
 }, {});
 
-let fieldsInOrder = Object.entries(fieldsToValues)
-  .map(([position, values]) => {
+// Two dimensional array with possible fields at every position
+// [
+//    ['arrival location', 'price', 'route', ...],
+//    ['departure location', 'departure station', 'departure platform', ...],
+//    ...
+// ]
+let possibleFieldsForPosition = Object.values(valuesForPosition)
+  .map((values) => {
     return fields.filter(([, fieldBoundaries]) => {
       const [
         lowerBoundaryFirstHalf,
@@ -73,42 +112,39 @@ let fieldsInOrder = Object.entries(fieldsToValues)
         upperBoundarySecondHalf,
       ] = fieldBoundaries;
 
-      const areAllValuesValid = values.map(Number).every((value) => {
+      return values.every((value) => {
         return (value >= lowerBoundaryFirstHalf && value <= upperBoundaryFirstHalf)
           || (value >= lowerBuondarySecondHalf && value <= upperBoundarySecondHalf);
       })
-
-      return areAllValuesValid;
     })
     .map(([fieldName]) => fieldName);
   });
 
-let fieldsInOrderCopy = [...fieldsInOrder];
-let hasMoreThanOneField = fieldsInOrderCopy.some((fields) => fields.length > 1);
 const fieldToPosition = {};
-while (hasMoreThanOneField) {
+let hasPositionWithMultipleFields = possibleFieldsForPosition.some((fields) => fields.length > 1);
+while (hasPositionWithMultipleFields) {
   // Find position with one field
-  const position = fieldsInOrderCopy.findIndex((fields) => fields.length === 1);
-
+  const position = possibleFieldsForPosition.findIndex((fields) => fields.length === 1);
   // Get single field for position
-  const [fieldForPosition] = fieldsInOrderCopy[position];
+  const [fieldForPosition] = possibleFieldsForPosition[position];
 
+  // Store mapping of field and discovered position
   fieldToPosition[fieldForPosition] = position;
 
-  fieldsInOrderCopy = fieldsInOrderCopy
-    .map((fields, index) => {
-      return position === index ? [] : fields.filter((field) => field !== fieldForPosition);
-    });
+  // Remove field from all other positions
+  possibleFieldsForPosition = possibleFieldsForPosition.map((fields, index) => {
+    return position === index ? [] : fields.filter((field) => field !== fieldForPosition);
+  });
 
-  hasMoreThanOneField = fieldsInOrderCopy.some((fields) => fields.length > 1);
+  // Do we still have positions with more than one field? If so, continue process of elimination
+  hasPositionWithMultipleFields = possibleFieldsForPosition.some((fields) => fields.length > 1);
 }
 
 const positionsForDepartureFields = Object.entries(fieldToPosition)
-  .filter(([fieldName, fieldPosition]) => fieldName.startsWith('departure'))
+  .filter(([fieldName]) => fieldName.startsWith('departure'))
   .map(([, fieldToPosition]) => fieldToPosition);
 
-const productOfValues = ourTicket.split('\n')[1].split(',')
-  .map(Number)
+const productOfValues = ourTicket
   .filter((value, index) => positionsForDepartureFields.includes(index))
   .reduce((acc, value) => acc * value, 1);
 
