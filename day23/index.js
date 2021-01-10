062c214dea9ea8dbd0dc5c062675ae0a75e3a1e9
Maxim Orlov <maximorl@gmail.com>
2021-01-10 15:23:55 +0100

    Day 23 part 2 solved!

--- day23/index.js
+++ day23/index.js
@@ -4,13 +4,13 @@
 const fs = require('fs');
 const path = require('path');
 const input = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8');
-
 const parsedCups = input.split('').map(Number);
-const highestCup = Math.max(...parsedCups);
 
 /* Part 1 */
 (() => {
   const cups = [...parsedCups];
+  const highestCup = Math.max(...parsedCups);
+
   let move = 1;
   let indexOfCurrentCup = 0;
   let currentCup = cups[indexOfCurrentCup];
@@ -45,6 +45,51 @@ const highestCup = Math.max(...parsedCups);
 
 /* Part 2 */
 (() => {
-  // TODO
-})();
+  const TOTAL_CUPS = 1_000_000;
+  const TOTAL_MOVES = 10_000_000;
 
+  // Create a singly linked list using an array where the index is the cup label
+  // and value is the next cup's label. We put a dummy value of 0 at the start so
+  // that `cup` = index (since array indeces start at 0)
+  // 389125467 is represented as [0, 2, 5, 8, 6, 4, 7, 3, 9, 1]
+  const cups = [0];
+  for (let cup = 1; cup <= TOTAL_CUPS; cup++) {
+    let nextCup;
+    if (cup <= parsedCups.length) {
+      nextCup = parsedCups[parsedCups.indexOf(cup) + 1] || parsedCups.length + 1;
+    } else if (cup === TOTAL_CUPS) {
+      nextCup = parsedCups[0];
+    } else {
+      nextCup = cup + 1;
+    }
+
+    cups[cup] = nextCup;
+  }
+
+  let move = 1;
+  let currentCup = parsedCups[0];
+  while (move <= TOTAL_MOVES) {
+    // Pick up three cups that are immediately clockwise of current cup
+    const cupOne = cups[currentCup];
+    const cupTwo = cups[cupOne];
+    const cupThree = cups[cupTwo];
+    cups[currentCup] = cups[cupThree];
+
+    // Select destination cup
+    let destinationCup = currentCup;
+    do {
+      // Cups with label below 1 don't exist so we wrap around to the highest label
+      destinationCup = destinationCup === 1 ? TOTAL_CUPS : destinationCup - 1;
+    } while ([cupOne, cupTwo, cupThree].includes(destinationCup))
+
+    // Place three cups immediately clockwise of destination cup
+    [cups[destinationCup], cups[cupThree]] = [cupOne, cups[destinationCup]];
+
+    // Select new current cup, which is immediately clockwise of current cup
+    currentCup = cups[currentCup];
+
+    move++;
+  }
+
+  console.log('Answer part 2:', cups[1] * cups[cups[1]]);
+})();
